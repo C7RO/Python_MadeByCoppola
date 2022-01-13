001d7b6903f4f34f741536419b3023c39def4b20
Carmine Coppola <90907848+C7RO@users.noreply.github.com>
2022-01-13 11:48:38 +0100

    Add files via upload

--- Natale2022/Coppola04.py
+++ Natale2022/Coppola04.py
@@ -6,9 +6,7 @@
 def formattaCampo(n):
     campo=[]
     for k in range(n):
-        riga =[]
-        for j in range(n):
-            riga.append(" ")
+        riga =[" "]*n
         campo.append(riga)
     return campo
 
@@ -27,7 +25,7 @@ def stampaCampo(campo, n):
 
 def is4(campo, r,c,n):
     win=False 
-    if r >= n-4:
+    if r >= 3:
         k=0
         win=True
         while k <= 3 and win == True:
@@ -35,7 +33,7 @@ def is4(campo, r,c,n):
                 win=False
             k+=1  
         if win==True: return True     
-        if c >= n-4:
+        if c >= 3:
             k=0
             win=True
             while k <= 3 and win == True:
@@ -50,32 +48,8 @@ def is4(campo, r,c,n):
                 if campo[r-k][c+k]!=campo[r][c]:
                     win=False
                 k+=1    
-            if win==True: return True     
-    if r <= n-4:
-        k=0
-        win=True
-        while k <= 3 and win == True:
-            if campo[r+k][c]!=campo[r][c]:
-                win=False
-            k+=1   
-        if win==True: return True            
-        if c >= n-4:   
-            k=0
-            win=True
-            while k <= 3 and win == True:
-                if campo[r+k][c-k]!=campo[r][c]:
-                    win=False
-                k+=1  
             if win==True: return True             
-        if c <= n-4:  
-            k=0
-            win=True
-            while k <= 3 and win == True:
-                if campo[r+k][c+k]!=campo[r][c]:
-                    win=False
-                k+=1    
-            if win==True: return True        
-    if c >= n-4:
+    if c >= 3:
         k=0
         win=True
         while k <= 3 and win == True:
@@ -94,52 +68,48 @@ def is4(campo, r,c,n):
     return win
 
 def controllaVittoria(campo, n, car):
-    righe=0
+    righe=n-1
     colonne=0
     vittoria=False
-    while righe < n and vittoria == False:
+    while righe >= 0 and vittoria == False:
         while colonne < n and vittoria == False:
             if campo[righe][colonne]==car:
                 vittoria=is4(campo,righe,colonne,n)
             colonne+=1
-        righe+=1
+        righe-=1
     return vittoria
 
-def isLibera(campo, n, r, c):
-    libera=False
-    if r >= 0 and r < n and c>= 0 and c < n:
-        if campo[r][c]==" ":
-            libera=True
-    return libera
+def isLibera(campo, n, c):
+    k=n-1
+    while (campo[k][c]=="X" or campo[k][c]=="0") and k >=0:
+        k-=1
+    return k
 
 n=5#Changing this little number you can choise the size of the map
 campo= formattaCampo(n)
 partitaFinita=False
 car1="X"
 car2="0"
-mossag1=[0,0]
-mossag2=[0,0]
+mossag1=0
+mossag2=0
 stampaCampo(campo,n)
 while(partitaFinita==False):
-    mossag1[0]=int(input("Giocatore 1 dammi la mossa(0-"+str(n-1)+")(0-"+str(n-1)+"): ")) #chiedere mossa g1
-    mossag1[1]=int(input("Colonna: "))
-    while(isLibera(campo,n,mossag1[0],mossag1[1])==False):  #verifica cella libera
-        print("Cella già occupata!!\n")
-        mossag1[0]=int(input("Giocatore 1 dammi la mossa(0-"+str(n-1)+")(0-"+str(n-1)+"): "))
-        mossag1[1]=int(input("Colonna: "))
-    campo[mossag1[0]][mossag1[1]]=car1
+    mossag1=int(input("Giocatore 1 dammi la mossa(0-"+str(n)+")")) #chiedere mossa g1
+    while isLibera(campo,n,mossag1) < 0:  #verifica cella libera
+        print("Colonna non disponibile\n")
+        mossag1=int(input("Giocatore 1 dammi la mossa(0-"+str(n)+")")) #chiedere mossa g1
+
+    campo[isLibera(campo,n,mossag1)][mossag1]=car1
     stampaCampo(campo,n)#stampa griglia
     if(controllaVittoria(campo,n,car1)):    #controlla Vittoria
         print("Complinti, il g1 ha vinto!!!")
         partitaFinita=True
     else:
-        mossag2[0]=int(input("Giocatore 2 dammi la mossa(0-"+str(n-1)+")(0-"+str(n-1)+"): ")) #chiedere mossa g1
-        mossag2[1]=int(input("Colonna: "))
-        while(isLibera(campo,n,mossag2[0],mossag2[1])==False):  #verifica cella libera
+        mossag2=int(input("Giocatore 2 dammi la mossa(0-"+str(n)+")")) #chiedere mossa g1
+        while(isLibera(campo,n,mossag2) < 0):  #verifica cella libera
             print("Cella già occupata!!\n")
-            mossag2[0]=int(input("Giocatore 2 dammi la mossa(0-"+str(n-1)+")(0-"+str(n-1)+"): "))
-            mossag2[1]=int(input("Colonna: "))
-        campo[mossag2[0]][mossag2[1]]=car2
+            mossag2=int(input("Giocatore 2 dammi la mossa(0-"+str(n)+")"))
+        campo[isLibera(campo,n,mossag2)][mossag2]=car2
         stampaCampo(campo,n)#stampa griglia
         if(controllaVittoria(campo,n,car2)):    #controlla Vittoria
             print("Complinti, il g2 ha vinto!!!")
